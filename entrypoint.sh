d86968534583ed5a96f2c899712dba2e975392b3
Sergio Garcez <s.garcez@thebeat.co>
2020-06-10 15:43:35 +0100

    Remove lost echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-echo
 if [[ "$#" = 1 ]]; then
     case $1 in
         --gen-script) cat /home/beat/bake-default.sh; exit 0 ;;
         --gen-bin) mage -goos linux -compile bake-build; exit 0 ;;
     esac
 fi
-
 
 if [ -z "${GITHUB_TOKEN}" ]; then 
     echo "[WARN] GITHUB_TOKEN is not set, you won't be able to access private repos."
