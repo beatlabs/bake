db3501ebf5655ec560fef24b5e75ee564a831cd8
Sergio Garcez <s.garcez@thebeat.co>
2020-06-10 15:37:56 +0100

    Add gen-script and gen-bin commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -z "${GITHUB_TOKEN}" ]; then echo GITHUB_TOKEN must be set; exit 1; fi
-git config --global url."https://$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
+set -e
+
+echo
+if [[ "$#" = 1 ]]; then
+    case $1 in
+        --gen-script) cat /home/beat/bake-default.sh; exit 0 ;;
+        --gen-bin) mage -goos linux -compile bake-build; exit 0 ;;
+    esac
+fi
+
+
+if [ -z "${GITHUB_TOKEN}" ]; then 
+    echo "[WARN] GITHUB_TOKEN is not set, you won't be able to access private repos."
+else
+    git config --global url."https://$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
+fi
 
 if [ -f $PWD/bake-build ]; then
     echo "executing prebuilt bake file"
