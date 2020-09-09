10ff128e309190d1ee1984ff6262cb08aee491a2
William Claude <w.claude@thebeat.co>
2020-09-09 16:26:29 +0200

    Add the possibility to run tests with extra args and to specify the pkg
    Signed-off-by: William Claude <w.claude@thebeat.co>

--- test/test.go
+++ test/test.go
@@ -11,24 +11,27 @@ import (
 )
 
 const (
-	coverFile = "coverage.txt"
+	// DefaultPkg to run test against.
+	DefaultPkg = "./..."
+	coverFile  = "coverage.txt"
 )
 
 // Run Go tests with cover and race flags enabled. Accepts extra args and build tags.
-func Run(extraArgs []string, tags ...string) error {
-	if len(tags) == 0 {
-		return run(extraArgs, nil)
-	}
-	return run(extraArgs, tags)
+func Run(extraArgs, tags []string, pkg string) error {
+	return run(extraArgs, tags, pkg)
 }
 
 // RunDefault Go tests with cover and race flags enabled and with default build tags.
 func RunDefault() error {
-	return run(nil, []string{bake.BuildTagIntegration, bake.BuildTagComponent})
+	return run(nil, []string{bake.BuildTagIntegration, bake.BuildTagComponent}, DefaultPkg)
 }
 
-func run(extraArgs, tags []string) error {
-	fmt.Printf("test: running tests with tags: %v\n", tags)
+func run(extraArgs, tags []string, pkg string) error {
+	if pkg == "" {
+		pkg = DefaultPkg
+	}
+
+	fmt.Printf("test: running tests with extra args: %v, tags: %v on pkg: %s\n", extraArgs, tags, pkg)
 
 	args := []string{
 		"test",
@@ -44,7 +47,7 @@ func run(extraArgs, tags []string) error {
 		args = append(args, getBuildTagFlag(tags))
 	}
 
-	args = append(args, "./...")
+	args = append(args, pkg)
 
 	fmt.Printf("Executing cmd: %s %s\n", bake.GoCmd, strings.Join(args, " "))
 
