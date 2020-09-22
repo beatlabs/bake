ef8f925051e4c18f0f407ce51ad1c4e9ea582c93
George Papadopoulos <g.papadopg@gmail.com>
2020-09-22 20:05:17 +0300

    add docker socket

--- bake.go
+++ bake.go
@@ -35,7 +35,8 @@ func RunDocker(img string, args ...string) error {
 	dockerArgs := []string{
 		"run", "--rm",
 		"--env", "GOFLAGS=-mod=vendor", "--env", "GO111MODULE=on",
-		"--volume", wd + ":/app", "--workdir", "/app", img, "sh", "-c",
+		"--volume", wd + ":/app", "--volume", "/var/run/docker.sock:/var/run/docker.sock",
+		"--workdir", "/app", img, "sh", "-c",
 		"git config --global url.\"https://golang:$GITHUB_TOKEN@github.com\".insteadOf \"https://github.com\" && " +
 			strings.Join(args, " ") +
 			" && chown --reference=/app --recursive /app",
