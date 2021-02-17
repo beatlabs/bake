9892337375961c0082288d87b710e31f749d3607
Sotirios Mantziaris <s.mantziaris@thebeat.co>
2021-02-17 11:06:05 +0200

    Coveralls in GH Actions (#81)
    Co-authored-by: Sergio Garcez <s.garcez@thebeat.co>

--- test/test.go
+++ test/test.go
@@ -3,7 +3,6 @@ package test
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/magefile/mage/sh"
@@ -68,13 +67,6 @@ func CoverDefault() error {
 
 func cover(tags []string) error {
 	fmt.Printf("test: running cover with tags: %v\n", tags)
-
-	defer func() {
-		err := os.Remove(coverFile)
-		if err != nil {
-			fmt.Printf("failed to delete coverage file: %v\n", err)
-		}
-	}()
 
 	args := []string{
 		"test",
