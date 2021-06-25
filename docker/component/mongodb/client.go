73a625f690d0872803736a1a864e3046be1565fa
Sergio Garcez <s.garcez@thebeat.co>
2021-06-25 14:01:34 +0100

    Mongo client direct connection
    Signed-off-by: Sergio Garcez <s.garcez@thebeat.co>

--- docker/component/mongodb/client.go
+++ docker/component/mongodb/client.go
@@ -10,8 +10,7 @@ import (
 // NewClient creates a new Client.
 func NewClient(ctx context.Context, address string) (*mongo.Client, error) {
 	opts := options.Client()
-	rs := "rs0"
-	opts.ReplicaSet = &rs
+	opts.SetDirect(true)
 	opts.ApplyURI("mongodb://" + address)
 
 	return mongo.Connect(ctx, opts)
