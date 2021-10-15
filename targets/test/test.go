934c5240a0f60c4ff226ed2947fe544f489159e1
Sotirios Mantziaris <smantziaris@gmail.com>
2021-10-15 16:32:00 +0300

    Upgrade dependencies

--- targets/test/test.go
+++ targets/test/test.go
@@ -26,7 +26,7 @@ var (
 		"-mod=vendor",
 		"-cover",
 		"-race",
-		"-shuffle on",
+		"-shuffle=on",
 	}
 	// CoverArgs used in coverage targets.
 	CoverArgs = []string{
@@ -36,7 +36,7 @@ var (
 		"-covermode=atomic",
 		"-coverprofile=coverage.txt",
 		"-race",
-		"-shuffle on",
+		"-shuffle=on",
 	}
 	// Pkgs is the pkg pattern to target.
 	Pkgs = "./..."
