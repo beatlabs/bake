601ae00b183460a9bdd1f05b4db2c4482b319cbb
Rodrigo Broggi <r.broggi@thebeat.co>
2021-10-18 16:12:41 +0200

    Diagrams integration (#171)
    * Integrating into bake ability to generate diagrams
    
    * First version of bake
    
    * Update diagram
    
    * Dockerfile lint
    
    * Golang lint
    
    * Golang lint
    
    * Renaming and commenting
    
    * comment on dependency

--- magefile.go
+++ magefile.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"github.com/taxibeat/bake/targets/diagram"
 	"github.com/taxibeat/bake/targets/lint/docker"
 	"github.com/taxibeat/bake/targets/test"
 
@@ -13,6 +14,8 @@ import (
 	_ "github.com/taxibeat/bake/targets/test"
 	// mage:import
 	_ "github.com/taxibeat/bake/targets/doc"
+	// mage:import
+	_ "github.com/taxibeat/bake/targets/diagram"
 	// mage:import
 	_ "github.com/taxibeat/bake/targets/lint/docker"
 	// mage:import
@@ -24,4 +27,6 @@ import (
 func init() {
 	docker.DockerFiles = []string{"./Dockerfile"}
 	test.CoverExcludePatterns = []string{"doc/", "docker/component/testservice/"}
+	// generate png diagrams for all *.py files in doc/ folder
+	diagram.InputDiagramPath = []string{"doc/"}
 }
