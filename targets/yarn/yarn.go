56c528a7f555dfa7ac686a458d938457a0483e99
Sergio Garcez <s.garcez@thebeat.co>
2021-11-04 12:15:24 +0000

    Try pre-script step
    Signed-off-by: Sergio Garcez <s.garcez@thebeat.co>

--- targets/yarn/yarn.go
+++ targets/yarn/yarn.go
@@ -11,18 +11,45 @@ import (
 
 const cmd = "yarn"
 
-var NpmToken string
+var (
+	NpmToken   string
+	Cwd        = "web"
+	TestSuffix = "--watchAll=false"
+)
 
 // Yarn groups yarn related targets.
 type Yarn mg.Namespace
 
-func (y Yarn) Script(script string) error {
-	if NpmToken != "" {
-		mg.SerialDeps(y.setToken)
+func (y Yarn) Install() error {
+	args := y.preScript()
+	return sh.RunV(cmd, args...)
+}
+
+func (y Yarn) Test() error {
+	args := y.preScript()
+
+	args = append(args, "test")
+
+	if TestSuffix != "" {
+		args = append(args, TestSuffix)
 	}
-	return sh.RunV(cmd, strings.Split(script, " ")...)
+
+	return sh.RunV(cmd, args...)
 }
 
 func (y Yarn) setToken() error {
 	return sh.RunV("npm", strings.Split(fmt.Sprintf("config set //registry.npmjs.org/:_authToken=%s", NpmToken), " ")...)
+}
+
+func (y Yarn) preScript() []string {
+	if NpmToken != "" {
+		mg.SerialDeps(y.setToken)
+	}
+
+	args := []string{}
+	if Cwd != "" {
+		args = append(args, "--cwd="+Cwd)
+	}
+
+	return args
 }
