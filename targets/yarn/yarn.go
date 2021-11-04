da4eb65aa82fb26126aad3a87e771d6d6fe89b77
Sergio Garcez <s.garcez@thebeat.co>
2021-11-04 14:52:56 +0000

    Docstrings
    Signed-off-by: Sergio Garcez <s.garcez@thebeat.co>

--- targets/yarn/yarn.go
+++ targets/yarn/yarn.go
@@ -12,25 +12,32 @@ import (
 const cmd = "yarn"
 
 var (
-	NpmToken    string
-	Cwd         = "web"
-	UnitTestCmd = "test --watchAll=false"
-	LintCmd     = "lint"
+	// NpmToken to be added to npm config in order to access private packages. Optional.
+	NpmToken string
+	// Cwd is the current working directory. Optional.
+	Cwd = "web"
+	// TestCmd is the test subcommand to be executed.
+	TestCmd = "test --watchAll=false"
+	// LintCmd is the lint subcommand to be executed.
+	LintCmd = "lint"
 )
 
 // Yarn groups yarn related targets.
 type Yarn mg.Namespace
 
+// Install runs the yarn install subcommand.
 func (y Yarn) Install() error {
 	return sh.RunV(cmd, y.prepScript()...)
 }
 
+// Install runs the yarn test subcommand.
 func (y Yarn) Test() error {
-	args := append(y.prepScript(), strings.Split(UnitTestCmd, " ")...)
+	args := append(y.prepScript(), strings.Split(TestCmd, " ")...)
 	mg.SerialDeps(y.Install)
 	return sh.RunV(cmd, args...)
 }
 
+// Lint runs the yarn lint subcommand.
 func (y Yarn) Lint() error {
 	args := append(y.prepScript(), strings.Split(LintCmd, " ")...)
 	mg.SerialDeps(y.Install)
