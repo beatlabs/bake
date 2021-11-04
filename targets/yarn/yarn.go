bf14c4fc6eea100cc5e4a2f97ee164543a14d0d7
Sergio Garcez <s.garcez@thebeat.co>
2021-11-04 11:55:12 +0000

    Support for NPM Token
    Signed-off-by: Sergio Garcez <s.garcez@thebeat.co>

--- targets/yarn/yarn.go
+++ targets/yarn/yarn.go
@@ -2,6 +2,7 @@
 package yarn
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/magefile/mage/mg"
@@ -10,9 +11,18 @@ import (
 
 const cmd = "yarn"
 
+var NpmToken string
+
 // Yarn groups yarn related targets.
 type Yarn mg.Namespace
 
 func (y Yarn) Script(script string) error {
+	if NpmToken != "" {
+		mg.SerialDeps(y.setToken)
+	}
 	return sh.RunV(cmd, strings.Split(script, " ")...)
+}
+
+func (y Yarn) setToken() error {
+	return sh.RunV("npm", strings.Split(fmt.Sprintf("config set //registry.npmjs.org/:_authToken=%s", NpmToken), " ")...)
 }
