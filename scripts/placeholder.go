13d438f9fffacf009a81ee747eb5aa88239864bc
Alexander Demin <alex@demins.com>
2022-07-01 11:23:31 +0300

    add package placeholder

---
+++ scripts/placeholder.go
@@ -0,0 +1,5 @@
+// Package scripts contains generic Bake bash scripts .
+package scripts
+
+// The purpose of this file is to allow a service to import this package with scripts inside
+// and run them via bash.sh or directly.
