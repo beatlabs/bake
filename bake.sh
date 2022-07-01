880e7ea5192c9d1d43e2485a9ea455f11641fce7
Alexander Demin <alex@demins.com>
2022-07-01 11:13:41 +0300

    move shell script inside the repo

--- bake.sh
+++ bake.sh
@@ -2,62 +2,27 @@
 
 set -e
 
-image_name="ghcr.io/taxibeat/bake"
-image_tag="latest"
+BAKE_RUN_SCRIPT=run-bake.sh
+BAKE_LOCATIONS=( "." "./scripts" "./vendor/github.com/taxibeat/bake/scripts" )
 
-# GID to be added to user groups in the running container
-# so that the user can interact with docker.
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  docker_sock="/var/run/docker.sock"
-  docker_gid=$(stat -c "%g" $docker_sock)
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  docker_sock="/var/run/docker.sock.raw"
-  docker_gid=0
-else
-  echo "Unsupported OS"
+for i in "${BAKE_LOCATIONS[@]}"; do
+  if [ ! -f "$i/$BAKE_RUN_SCRIPT" ]; then
+    continue
+  fi
+
+  echo "Using bake run script at $i/$BAKE_RUN_SCRIPT"
+
+  if [ ! -x "$i/$BAKE_RUN_SCRIPT" ]; then
+      echo "Bake script is not executable, applying chmod..."
+      chmod u+x "$i/$BAKE_RUN_SCRIPT"
+  fi
+
+  SCRIPT_FOUND=1
+  "$i/$BAKE_RUN_SCRIPT" "$@"
+done
+
+if [ -z $SCRIPT_FOUND ]; then
+  echo Bake run script does not exist in any of these locations "${BAKE_LOCATIONS[*]}"
   exit 1
 fi
 
-BAKE_SESSION_ID=$(uuidgen | cut -c 1,2,3 | tr "[:upper:]" "[:lower:]")
-BAKE_NETWORK_ID=$(docker network create "${BAKE_SESSION_ID}")
-printf "Bake Session ID: $BAKE_SESSION_ID\nBake Network ID: $BAKE_NETWORK_ID\n\n"
-
-cleanup () {
-  logsdir='.bake-container-logs'
-  mkdir -p $logsdir
-  docker ps --format '{{.Names}}' | grep "^$BAKE_SESSION_ID-" | awk '{print $1}' | xargs -I {} sh -c "docker logs {} > $logsdir/{}.log 2>&1"
-  docker ps --format '{{.Names}}' | grep "^$BAKE_SESSION_ID-" | awk '{print $1}' | xargs -I {} docker rm -f {} > /dev/null
-  # docker image list --format '{{.Repository}}' | grep "^$BAKE_SESSION_ID-" | awk '{print $1}' | xargs -I {} docker rmi -f {} > /dev/null
-  docker image list --format '{{.Repository}}:{{.Tag}}' | grep ":$BAKE_SESSION_ID\$" | awk '{print $1}' | xargs -I {} docker rmi -f {} > /dev/null
-  docker network rm "$BAKE_NETWORK_ID" > /dev/null
-  rm -f docker/component/.bakesession > /dev/null
-  echo "Bake cleanup complete"
-}
-
-if [[ "$SKIP_CLEANUP" != "1" ]]; then
-trap cleanup EXIT
-fi
-
-# Detect TTY
-[[ -t 1 ]] && tty='--tty'
-
-docker run \
-  --name "$BAKE_SESSION_ID-bake" \
-  --network $BAKE_NETWORK_ID \
-  $tty \
-  --rm \
-  --user $(id -u):$(id -g) \
-  --group-add $docker_gid \
-  --volume ${docker_sock}:/var/run/docker.sock \
-  --volume "$PWD":/src \
-  --workdir /src \
-  --env BAKE_NETWORK_ID="$BAKE_NETWORK_ID" \
-  --env BAKE_SESSION_ID="${BAKE_SESSION_ID}" \
-  --env GITHUB_TOKEN="$GITHUB_TOKEN" \
-  --env CONFLUENCE_USERNAME="$CONFLUENCE_USERNAME" \
-  --env CONFLUENCE_PASSWORD="$CONFLUENCE_PASSWORD" \
-  --env CONFLUENCE_BASEURL="$CONFLUENCE_BASEURL" \
-  --env GITHUB_ACTIONS="$GITHUB_ACTIONS" \
-  --env BAKE_PUBLISH_PORTS="true" \
-  $image_name:$image_tag \
-  $@
---
+++ scripts/run-bake.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -e
+
+image_name="ghcr.io/taxibeat/bake"
+image_tag=`cat go.mod | grep github.com/taxibeat/bake | cut -f2 -d"v"`
+
+# "module" value means that this script is used in the Bake repository itself
+if [ "$image_tag" == "module github.com/taxibeat/bake" ]; then
+  echo "Setting bake image tag to latest"
+  image_tag="latest"
+fi
+
+if ! [[ `cat ${HOME}/.docker/config.json | grep ghcr.io` ]]; then
+  echo "docker config not found for ghcr.io, please log in"
+fi
+
+# GID to be added to user groups in the running container
+# so that the user can interact with docker.
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  docker_sock="/var/run/docker.sock"
+  docker_gid=$(stat -c "%g" $docker_sock)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  docker_sock="/var/run/docker.sock.raw"
+  docker_gid=0
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
+BAKE_SESSION_ID=$(uuidgen | cut -c 1,2,3 | tr "[:upper:]" "[:lower:]")
+BAKE_NETWORK_ID=$(docker network create "${BAKE_SESSION_ID}")
+printf "Bake Session ID: $BAKE_SESSION_ID\nBake Network ID: $BAKE_NETWORK_ID\n\n"
+
+cleanup () {
+  logsdir='.bake-container-logs'
+  mkdir -p $logsdir
+  docker ps --format '{{.Names}}' | grep "^$BAKE_SESSION_ID-" | awk '{print $1}' | xargs -I {} sh -c "docker logs {} > $logsdir/{}.log 2>&1"
+  docker ps --format '{{.Names}}' | grep "^$BAKE_SESSION_ID-" | awk '{print $1}' | xargs -I {} docker rm -f {} > /dev/null
+  # docker image list --format '{{.Repository}}' | grep "^$BAKE_SESSION_ID-" | awk '{print $1}' | xargs -I {} docker rmi -f {} > /dev/null
+  docker image list --format '{{.Repository}}:{{.Tag}}' | grep ":$BAKE_SESSION_ID\$" | awk '{print $1}' | xargs -I {} docker rmi -f {} > /dev/null
+  docker network rm "$BAKE_NETWORK_ID" > /dev/null
+  rm -f docker/component/.bakesession > /dev/null
+  rm -f test/.bakesession > /dev/null
+  echo "Bake cleanup complete"
+}
+
+if [[ "$SKIP_CLEANUP" != "1" ]]; then
+trap cleanup EXIT
+fi
+
+# Detect TTY
+[[ -t 1 ]] && tty='--tty'
+
+docker run \
+  --name "$BAKE_SESSION_ID-bake" \
+  --network $BAKE_NETWORK_ID \
+  $tty \
+  --rm \
+  --user $(id -u):$(id -g) \
+  --group-add $docker_gid \
+  --volume ${docker_sock}:/var/run/docker.sock \
+  --volume "$PWD":/src \
+  --workdir /src \
+  --env BAKE_NETWORK_ID="$BAKE_NETWORK_ID" \
+  --env BAKE_SESSION_ID="${BAKE_SESSION_ID}" \
+  --env GITHUB_TOKEN="$GITHUB_TOKEN" \
+  --env CONFLUENCE_USERNAME="$CONFLUENCE_USERNAME" \
+  --env CONFLUENCE_PASSWORD="$CONFLUENCE_PASSWORD" \
+  --env CONFLUENCE_BASEURL="$CONFLUENCE_BASEURL" \
+  --env GITHUB_ACTIONS="$GITHUB_ACTIONS" \
+  --env BAKE_PUBLISH_PORTS="true" \
+  $image_name:$image_tag \
+  $@
