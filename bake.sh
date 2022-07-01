c43715fb7f0b8c1fd0f8bffc3e66f445e77101e9
Alexander Demin <alex@demins.com>
2022-07-01 12:50:34 +0300

    simplify bake.sh

--- bake.sh
+++ bake.sh
@@ -2,27 +2,4 @@
 
 set -e
 
-BAKE_RUN_SCRIPT=run-bake.sh
-BAKE_SCRIPT_LOCATIONS=( "./scripts" "./vendor/github.com/taxibeat/bake/scripts" )
-
-for i in "${BAKE_SCRIPT_LOCATIONS[@]}"; do
-  if [ ! -f "$i/$BAKE_RUN_SCRIPT" ]; then
-    continue
-  fi
-
-  echo "Using bake run script at $i/$BAKE_RUN_SCRIPT"
-
-  if [ ! -x "$i/$BAKE_RUN_SCRIPT" ]; then
-      echo "Bake script is not executable, applying chmod..."
-      chmod u+x "$i/$BAKE_RUN_SCRIPT"
-  fi
-
-  SCRIPT_FOUND=1
-  "$i/$BAKE_RUN_SCRIPT" "$@"
-  break
-done
-
-if [ -z $SCRIPT_FOUND ]; then
-  echo Bake run script does not exist in any of these locations "${BAKE_SCRIPT_LOCATIONS[*]}"
-  exit 1
-fi
+bash ./scripts/run-bake.sh "$@"
